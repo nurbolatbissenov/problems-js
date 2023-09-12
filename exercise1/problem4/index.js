8ef44c0cfaa7384348c64867b136d97acb69d1c9
Nurbolat M. Bissenov <BissenovNM@school121.kz>
2023-09-12 14:59:38 +0600

    problem4

--- exercise1/problem4/index.js
+++ exercise1/problem4/index.js
@@ -1,5 +1,10 @@
-function makeTitleCase() {
-  // Your code
+function makeTitleCase(s) {
+  return s.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
+
+console.log(makeTitleCase("This is a title")) // "This Is A Title"
+console.log(makeTitleCase("capitalize every word")) // "Capitalize Every Word"
+console.log(makeTitleCase("I Like Pizza")) // "I Like Pizza"
+console.log(makeTitleCase("PIZZA PIZZA PIZZA")) // "PIZZA PIZZA PIZZA"
 
 module.exports = makeTitleCase;
