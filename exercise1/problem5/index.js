21a78c9178dc6e379ea2945e0fc02285fcfc3db9
Nurbolat M. Bissenov <BissenovNM@school121.kz>
2023-09-12 16:08:19 +0600

    problem5

--- exercise1/problem5/index.js
+++ exercise1/problem5/index.js
@@ -1,5 +1,12 @@
-function removeABC() {
-  // Your code
+function removeABC(str) {
+  if (!/[abc]/.test(str))  {
+      return null;
+  }
+  return str.replace(/[abc]/g, '');
 }
+
+console.log(removeABC("This might be a bit hard")) // "This might e  it hrd"
+console.log(removeABC("hello world!")) // null
+console.log(removeABC("")) // null
 
 module.exports = removeABC;
